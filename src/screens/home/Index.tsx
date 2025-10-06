9b7714998d7b6a862f211539178fb1e549280034
NEELAM GAYKWAD <neelg1805@gmail.com>
2025-10-06 06:30:58 +0530

    ads

--- src/screens/home/Index.tsx
+++ src/screens/home/Index.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Text, View, TouchableOpacity } from 'react-native'
 import Header from '../../components/header/Index'
 import SafeAreaViewWrapper from '../../components/safeAreaViewWrapper/Index'
+import { BannerAd, BannerAdSize, TestIds } from 'react-native-google-mobile-ads';
+const adUnitId = __DEV__ ? TestIds.ADAPTIVE_BANNER : 'ca-app-pub-xxxxxxxxxxxxx/yyyyyyyyyyyyyy';
 
 function Index({ navigation }: any) {
   return (
@@ -10,6 +12,15 @@ function Index({ navigation }: any) {
       <View style={{ flex: 1 }}>
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
           <Text>Home Screen</Text>
+          <BannerAd
+            unitId={adUnitId}
+            size={BannerAdSize.ANCHORED_ADAPTIVE_BANNER}
+            requestOptions={{
+              networkExtras: {
+                collapsible: 'bottom',
+              },
+            }}
+          />
           <TouchableOpacity
             style={{
               backgroundColor: '#007AFF',
